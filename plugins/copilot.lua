3c1af8aa20ceb1ccfad58d5480a64081b384d50b
江口純矢 <s_eguchi@codmon.co.jp>
2025-04-25 09:42:32 +0900

    fix: kdfjdkfj

--- plugins/copilot.lua
+++ plugins/copilot.lua
@@ -1,4 +1,13 @@
 return {
-  "github/copilot.vim",
-  lazy=false,
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			filetypes = {
+				markdown = true, -- ← 補完を有効にする
+				help = false, -- ← 無効にしたい例
+			},
+		})
+	end,
 }
