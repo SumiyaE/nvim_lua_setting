34c9b661cfaa912c3bc4fc28e261b8229cdb2bf5
江口純矢 <s_eguchi@codmon.co.jp>
2025-05-07 14:57:35 +0900

    refactor: ファイルパスが変更されるように変更

--- plugins/lualine.lua
+++ plugins/lualine.lua
@@ -1,4 +1,26 @@
+-- lua/plugins/lualine.lua
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+				icons_enabled = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = {
+					{
+						"filename",
+						path = 1, -- 0: filename only, 1: relative path, 2: absolute path
+					},
+				},
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
