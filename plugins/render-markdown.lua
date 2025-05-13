c7b3d51c079957ab9923b364276b53fb9075c465
江口純矢 <s_eguchi@codmon.co.jp>
2025-05-13 10:59:35 +0900

    feat: markdow-renderをavanteの依存から削除

--- plugins/render-markdown.lua
+++ plugins/render-markdown.lua
@@ -1,9 +1,20 @@
 return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {},
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {
+		win_options = {
+			-- @see :h 'conceallevel'
+			conceallevel = {
+				-- Used when not being rendered, get user setting.
+				-- default = vim.o.conceallevel,
+				default = 0,
+				-- Used when being rendered, concealed text is completely hidden.
+				rendered = 0,
+			},
+		},
+	},
 }
