bc8b50967388ba8bb08d71ff6adfddae0f04186a
SumiyaE <s_eguchi@codmon.co.jp>
2025-11-06 08:00:53 +0900

    fix:

--- plugins/nightfox.lua
+++ plugins/nightfox.lua
@@ -5,6 +5,13 @@ return {
 		vim.cmd("highlight! link WinSeparator GlyphPalette2")
 		vim.cmd("highlight! Visual guibg=#4a3332")
 
+		-- アクティブなウィンドウを明るくする
+		vim.cmd("highlight! Normal guibg=#2e3440 guifg=#d8dee9")
+
+		-- アクティブなウィンドウのカーソル行をより明るく
+		vim.cmd("highlight! CursorLine guibg=#3b4252")
+		vim.cmd("highlight! CursorLineNr guifg=#88c0d0 gui=bold")
+
 		-- 非アクティブなウィンドウをかなり暗くする（背景と文字色）
 		vim.cmd("highlight! NormalNC guibg=#1a1f28 guifg=#5a657d")
 
