57c0fec2646e0eee79bbfa217edea4de9d575052
SumiyaE <s_eguchi@codmon.co.jp>
2025-11-06 16:55:56 +0900

    fix: LSP設定をNeovim 0.11+のvim.lsp.config APIに移行

--- plugins/lsp/mason.lua
+++ plugins/lsp/mason.lua
@@ -1,42 +1,32 @@
 return {
 	"williamboman/mason.nvim",
 	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
 		"neovim/nvim-lspconfig",
+		"williamboman/mason-lspconfig.nvim",
 	},
 	config = function()
 		-- mason自体のセットアップ
 		require("mason").setup()
 
-		-- mason-lspconfigのセットアップ
+		-- デフォルトのcapabilitiesを全サーバーに適用
+		vim.lsp.config["*"] = {
+			capabilities = require("cmp_nvim_lsp").default_capabilities(),
+		}
+
+		-- lua_ls専用の設定
+		vim.lsp.config.lua_ls = {
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		}
+
+		-- mason-lspconfigのセットアップ（自動でvim.lsp.enable()を呼び出す）
 		require("mason-lspconfig").setup({
 			ensure_installed = { "lua_ls", "terraformls", "marksman", "clangd" },
-		})
-
-		-- デフォルトのcapabilitiesを取得
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-		-- lspサーバーを自動で設定
-		require("mason-lspconfig").setup_handlers({
-			-- デフォルトハンドラー
-			function(server_name)
-				require("lspconfig")[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-			-- lua_ls専用の設定
-			["lua_ls"] = function()
-				require("lspconfig").lua_ls.setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
-					},
-				})
-			end,
 		})
 	end,
 }
